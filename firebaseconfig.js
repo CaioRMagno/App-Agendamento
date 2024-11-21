7db9330ec4134ed2f96ec2d2b19a8ed7dc9a0ca5
CaioRMagno <caiormagno@gmail.com>
2024-11-21 12:53:01 -0300

    Atualiza configuração do Firebase e corrige erro de inicialização

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,35 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-// Variáveis de ambiente
-import { 
-  REACT_APP_FIREBASE_API_KEY, 
-  REACT_APP_FIREBASE_AUTH_DOMAIN, 
-  REACT_APP_FIREBASE_PROJECT_ID, 
-  REACT_APP_FIREBASE_STORAGE_BUCKET, 
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID, 
-  REACT_APP_FIREBASE_APP_ID, 
-  REACT_APP_FIREBASE_MEASUREMENT_ID 
-} from 'react-native-dotenv';
+import Constants from 'expo-constants';
 
 // Configuração do Firebase usando variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyDS5hBUXYI4nw55qT-FqEUvZAqdLyXtaRM",
-  authDomain: "agendamento-2ee56.firebaseapp.com",
-  projectId: "agendamento-2ee56",
-  storageBucket: "agendamento-2ee56.appspot.com", // Corrigido
-  messagingSenderId: "1038770177663",
-  appId: "1:1038770177663:web:92f59f309d23279a41e14a",
-  measurementId: "G-D8EZXW1KTV",
+  apiKey: Constants.manifest.extra.FIREBASE_API_KEY,
+  authDomain: Constants.manifest.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.manifest.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.manifest.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.manifest.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.manifest.extra.FIREBASE_APP_ID,
+  measurementId: Constants.manifest.extra.FIREBASE_MEASUREMENT_ID,
 };
 
-// Inicializa o Firebase apenas uma vez
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
 // Inicializa os serviços do Firebase
-const auth = getAuth(app);         // Instância do Firebase Auth
-const db = getFirestore(app);      // Instância do Firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 // Exporta as instâncias para uso no projeto
 export { app, auth, db, firebaseConfig };
